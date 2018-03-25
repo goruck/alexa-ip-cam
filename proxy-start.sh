c26ea20d56afa1f5417247f95e59bfd6bb8fa53e
goruck <lindo.st.angel@gmail.com>
2018-03-25 05:39:55 -0700

    added back porch camera

--- proxy-start.sh
+++ proxy-start.sh
@@ -2,7 +2,7 @@
 
 # Read camera credentials and IP addresses.
 # e.g., alexa:secret-passwd@192.168.1.15
-. /home/lindo/dev/show-cameras/vars.sh
+. /home/lindo/dev/alexa-ip-cam/vars.sh
 
 # Start the proxy server.
 # Streams will be at rtsp://HOST_IP:554/proxyStream-n
@@ -10,7 +10,8 @@
 /home/lindo/dev/live/proxyServer/live555ProxyServer \
   rtsp://$CAM_1/onvif-media/media.amp?profile=profile_alexa_h264 \
   rtsp://$CAM_2/onvif-media/media.amp?profile=profile_alexa_h264 \
-  rtsp://$CAM_3/onvif-media/media.amp?profile=profile_alexa_h264 \
+  "rtsp://$CAM_3/onvif-media/media.amp?profile=profile_alexa_h264&gotopresetname=Home&camera=2" \
   rtsp://$CAM_4/onvif-media/media.amp?profile=profile_alexa_h264 \
   rtsp://$CAM_5/onvif-media/media.amp?profile=profile_alexa_h264 \
-  rtsp://$CAM_6/onvif-media/media.amp?profile=profile_alexa_h264
+  rtsp://$CAM_6/onvif-media/media.amp?profile=profile_alexa_h264 \
+  "rtsp://$CAM_3/onvif-media/media.amp?profile=profile_alexa_h264&gotopresetname=Home&camera=1"
