41dda175df936e174461ae9af755cea363da8ef5
Lindo St. Angel <lindo.st.angel@gmail.com>
2018-08-24 04:50:58 -0700

    updated paths

--- proxy-start.sh
+++ proxy-start.sh
@@ -2,12 +2,12 @@
 
 # Read camera credentials and IP addresses.
 # e.g., alexa:secret-passwd@192.168.1.15
-. /home/lindo/dev/alexa-ip-cam/vars.sh
+. /home/lindo/develop/alexa-ip-cam/vars.sh
 
 # Start the proxy server.
 # Streams will be at rtsp://HOST_IP:554/proxyStream-n
 # where n is the order of the input streams below.
-/home/lindo/dev/live/proxyServer/live555ProxyServer \
+/home/lindo/develop/live/proxyServer/live555ProxyServer \
   rtsp://$CAM_1/onvif-media/media.amp?profile=profile_alexa_h264 \
   rtsp://$CAM_2/onvif-media/media.amp?profile=profile_alexa_h264 \
   "rtsp://$CAM_3/onvif-media/media.amp?profile=profile_alexa_h264&gotopresetname=Home&camera=2" \
